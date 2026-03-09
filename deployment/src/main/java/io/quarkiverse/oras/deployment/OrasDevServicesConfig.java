45783b91e195970fe333a0a2f34fba5038e6169b
Valentin Delaye <jonesbusy@users.noreply.github.com>
2026-03-09 11:37:36 +0100

    Update zot to 2.1.15 for devservices

--- deployment/src/main/java/io/quarkiverse/oras/deployment/OrasDevServicesConfig.java
+++ deployment/src/main/java/io/quarkiverse/oras/deployment/OrasDevServicesConfig.java
@@ -15,7 +15,7 @@ public interface OrasDevServicesConfig {
     /**
      * The container image name to use, for the Zot registry
      */
-    @WithDefault("ghcr.io/project-zot/zot-linux-amd64:v2.1.14")
+    @WithDefault("ghcr.io/project-zot/zot-linux-amd64:v2.1.15")
     String imageName();
 
     /**
